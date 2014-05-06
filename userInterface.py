b8a209a8cdb30a9c3963351df6016db42af45cf7
Renee Spangler <renees2@vt.edu>
2014-05-06 17:19:19 -0400

    Added some error checking and more functionality

--- userInterface.py
+++ userInterface.py
@@ -1,55 +1,68 @@
 #!/usr/bin/env python3
 
 import sys
+from random import randint
 
 print ('\n')
 print ("Welcome to the world.  Here you must clean the world of monsters.  Each monster will have a weapon on varying power.  Take care, as the monster might have more strength than you.  Remember retreat is a viable option.")
 
 print ('\n')
 
+weapon = randint(0,4)
+
+
+for i in range(0,4):
+	num = randint(0,4)
+
 
 #Initialize monsters
 monsterEngaged = False
-command = input("Please enter one of the following commands: monster, attack, defend, retreat, pick up,  help, stats.  The help command lists what each command does.  ")
-print ("To quit type: quit")
+monsterDead = False
+MonsterCount = 0
+gameOver = False
+command = input("Please enter one of the following commands: monster, attack, defend, retreat, pick up,  help, stats.  The help command lists what each command does. To quit, type quit  ")
 while command != "quit":
 	print ('\n')
-	if command == "monster":
+	if command == "monster":#Engages the monster
 		if monsterEngaged == False:
-			print ("You are now engaged with this monster:")
+			print ("You are now engaged with this monster:", '\n')
 			print ("Name: ", '\n')
 			print ("Health: ", '\n')
 			print ("Weapon: ", '\n')
 			monsterEngaged = True
+			monsterDead = False
 			#Call function to engage monster
 		else:
 			print ("You are already engaged with a monster.  Please enter a new command.", '\n')
-	elif command == "attack":
+	elif command == "attack": #Attacks the monster unless not engaged with one
 		if monsterEngaged == False:
 			print("You are not engaged with a monster. Enter a new command.", '\n')
 		else:
 			#Call attack function
-			print ("Your health, monster health", '\n')
-	elif command == "defend":
+			print ("Your health: ", '\n')
+			print ("Monster health: ", '\n')
+	elif command == "defend": #Defends against the monster's attack unless not engaged with one
 		if monsterEngaged == False:
 			print ("You are not engaged with a monster. Enter a new command.", '\n')
 		else:
 			#Call defend function
 			print ("SHIELD SHIELD!", '\n')
-	elif command == "retreat":
+			print ("Your health: ", '\n')
+			print ("Monster health: ", '\n')
+	elif command == "retreat": #Retreats from the battle
 		if monsterEngaged == False:
 			print("You are not engaged with a monster. Please enter a new command", '\n')
 		else:
 			#Call disengage function
 			monsterEngaged = False
 			print ("When you fight and run away....You live to fight another day.", '\n')
-	elif command == "stats":
+	elif command == "stats": #Displays your health and weapon, and if you are fighting a monster, displays its health and weapon
 		print ("Your health: ", '\n')
 		print ("Your weapon: ", '\n')
 		if monsterEngaged == True:
 			print ("Monster's Health: ", '\n')
 			print ("Monster's Weapon: ", '\n')
-	elif command == "help":
+	elif command == "help":#Outputs all of the commangs and what they do
 		print ("monster    - engages a monster in combat", '\n')
 		print ("attack     - attacks the monster", '\n')
 		print ("defend     - defends against the monster's next attack", '\n')
@@ -58,16 +71,16 @@ while command != "quit":
 		print ("stats      - displays your statistics and those of the monster you are currently fighting", '\n')
 		print ("pick up    - picks up the defeated monster's weapon", '\n')
 		print ("quit       - exits the game", '\n')
-	elif command == "pick up":
+	elif command == "pick up":#Also need to scheck if monster is dead
 		if monsterEngaged == False:
 			print ("You are not engaged with a monster, or the monster is not yet dead so there is no weapon to pick up.", '\n')
 		else:
 			print ("You now have this weapon", '\n')
+	elif command == "What is the answer to life, the universe, and everything?":
+		print ("42", '\n')
 	else:
-		print (command)
+		print ("Invalid Command.  Please use: monster, attack, defend, retreat, help, stats, pick up, or quit", '\n')
 	
 	command = input("Command:   ")
-
-#If command = monster go to first monster in the list
-#if command = attact, defend, retreat
-#if monster is dead, ask to pick up weapon?
+	if gameOver == True:
+		command = "quit"
